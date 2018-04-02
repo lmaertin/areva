07cf3a5292901d78406d2b4d7f5a4600274dd04f
maertin <maertin@merry.fritz.box>
2018-04-02 23:26:25 +0200

    cleanup for new release

--- de.tubs.areva.annotations/.svn/pristine/df/dfa51ac5146c672d27289bfdb608b8a6a13da23d.svn-base
+++
@@ -1,5 +0,0 @@
-package de.tubs.areva.annotations;
-
-public @interface ExportMethod {
-	
-}
--- de.tubs.areva/src/de/tubs/forjahn/bachelor/calculations/Centrality.java
+++
@@ -1,7 +0,0 @@
-package de.tubs.forjahn.bachelor.calculations;
-
-public enum Centrality {
-	DEGREE,
-	BETWEEN,
-	CLOSENESS
-}
